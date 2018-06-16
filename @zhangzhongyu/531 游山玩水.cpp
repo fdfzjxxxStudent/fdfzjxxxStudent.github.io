745c60a5334c5cb0cbc3972f43b022eef4964962
A Student of fdfzjxxx <36366057+fdfzjxxxStudent@users.noreply.github.com>
2018-06-16 15:07:13 +0800

    @BobbyZZY

---
+++ @zhangzhongyu/531 游山玩水.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<algorithm>
+#define N 25
+#define INF 10000
+using namespace std;
+int dx[4]={0,1,0,-1};
+int dy[4]={1,0,-1,0};
+int n,m,dist[N][N];
+char f[N][N];
+void dfs(int x,int y,int len){
+	if(len>=dist[x][y])return;
+	dist[x][y]=len;
+	if(x==n&&y==m)return;
+	for(int k=0;k<4;k++){
+		int nx=x+dx[k],ny=y+dy[k];
+		if(nx>=1&&nx<=n&&ny>=1&&ny<=m&&f[nx][ny]==f[x][y])
+			dfs(nx,ny,len);
+	}
+	for(int k=0;k<4;k++){
+		int nx=x+dx[k],ny=y+dy[k];
+		if(nx>=1&&nx<=n&&ny>=1&&ny<=m&&f[nx][ny]!=f[x][y])
+			dfs(nx,ny,len+1);
+	}
+}
+int main(){
+	cin>>n>>m;
+	for(int i=1;i<=n;i++)
+		for(int j=1;j<=m;j++)
+			cin>>f[i][j];
+	fill(dist[0],dist[0]+N*N,INF);
+	dfs(1,1,0);
+	int ans=dist[n][m];
+	cout<<ans<<endl;
+	return 0;
+}
+
+
